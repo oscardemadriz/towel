eeaac7b3bf2de8437b9d7ff32dcea5e0ceaf6b9d
oscardemadriz <35626977+oscardemadriz@users.noreply.github.com>
2018-03-17 13:08:31 -0700

    towel v01

---
+++ towel.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+ 
+display_usage() {
+	echo "# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    towel.sh                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ossanche <oscardemadriz@gmail.com>         +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/03/17 10:42:06 by ossanche          #+#    #+#              #
+#    Updated: 2018/03/17 10:42:34 by ossanche         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #" 
+	echo -e "This script needs day number and intra user name.\nWill download Stupidity and your repository code from dayXX,\nCheck with norminette, build them and also test them with Stupidity" 
+	echo  -e "\nUsage:\n ./towel.sh 03 ossanche \n" 
+	} 
+
+# if less than two arguments supplied, display usage 
+	if [  $# -le 1 ] 
+	then 
+		display_usage
+		exit 1
+	fi 
+ 
+# check whether user had supplied -h or --help . If yes display usage 
+	if [[ ( $# == "--help") ||  $# == "-h" ]] 
+	then 
+		display_usage
+		exit 0
+	fi 
+
+git clone https://github.com/mirror12k/42us-stupidity.git stupid
+cd stupid
+git clone vogsphere@vgs.42.us.org:intra/2018/activities/piscine_c_day_03/$2 day$1
+./spawn.pl day$1 config_d$1.pl
+./tools/build.sh
+./tools/verify.sh
+./tools/check_all.sh
+rm -rf stupid
